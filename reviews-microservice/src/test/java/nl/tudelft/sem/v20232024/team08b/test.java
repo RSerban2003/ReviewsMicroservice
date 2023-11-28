9f7b2a5b0f82bac18e86c182f259916ea64f73e3
RSerban2003 <rares.serban2003+git@gmail.com>
2023-11-28 14:18:58 +0100

    comment

--- reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/test.java
+++ reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/test.java
@@ -5,6 +5,9 @@ import org.junit.jupiter.api.Test;
 public class test {
     @Test
     void test() {
-
+        /*
+        This is a temporary dummy test that is required for Jacoco
+        to generate the report file.
+        */
     }
 }
