ee8005e5f3521a19fabfcf9aaec72bdf6f72e778
vmitev <V.Mitev@student.tudelft.nl>
2023-11-28 16:05:37 +0100

    Added a NEUTRAL option to bids

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/TrackController.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/TrackController.java
@@ -1,21 +1,27 @@
 package nl.tudelft.sem.v20232024.team08b.controllers;
 
-import java.util.Date;
-
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.Date;
 import nl.tudelft.sem.v20232024.team08b.dtos.TrackAnalytics;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/conferences/{conferenceID}/tracks/{trackID}")
-@Tag(name = "Tracks", description = "Operations for dealing with tracks: getting summaries and setting deadlines")
+@Tag(name = "Conference Tracks", description = "Operations for dealing with conference tracks: getting summaries and setting deadlines")
 public class TrackController {
 
   @Operation(summary = "Get the summary stats of a track",
--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/dtos/Bid.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/dtos/Bid.java
@@ -2,8 +2,10 @@ package nl.tudelft.sem.v20232024.team08b.dtos;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 
-@Schema(description = "Specifies a preference by the reviewer to review / not review a paper")
+@Schema(description = "Specifies a preference by the reviewer to review / not review a paper " +
+    "(based on their expertise). By default, every reviewer is NEUTRAL towards a paper.")
 public enum Bid {
-    WANT,
-    DONTWANT
+    CANREVIEW,
+    NEUTRAL,
+    NOTREVIEW
 }
