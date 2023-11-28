558dc6cfd29f337a2f9fb46c26871e5fddd36398
vmitev <V.Mitev@student.tudelft.nl>
2023-11-28 11:40:54 +0100

    Fixed the ConfidentialCommentCntrl

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/ConfidentialCommentCntrl.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/ConfidentialCommentCntrl.java
@@ -1,12 +1,16 @@
 package nl.tudelft.sem.v20232024.team08b.controllers;
 
+import java.util.UUID;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.UUID;
-
-@RestController("/paper/{paperID}/confidentialComment")
+@RestController
+@RequestMapping("/paper/{paperID}/confidentialComment")
 public class ConfidentialCommentCntrl {
     /**
      * @param paperID the ID for the paper
