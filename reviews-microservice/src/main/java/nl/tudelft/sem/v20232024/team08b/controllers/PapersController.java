138a8df8afc0cdac2a66c940146403bba2ed3511
jrzwrz <j.k.wierzbicki@student.tudelft.nl>
2023-11-30 09:38:55 +0100

    Added descriptions to the parameters and changed response descriptions

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/PapersController.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/controllers/PapersController.java
@@ -1,6 +1,7 @@
 package nl.tudelft.sem.v20232024.team08b.controllers;
 
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
@@ -24,40 +25,38 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Papers", description = "Operations for viewing papers -- their contents, review status, etc.")
 public class PapersController {
     @Operation(summary = "Get the title and abstract of a paper",
-        description = "Responds with the title and abstract of the given paper. " +
-            "The requester must be a reviewer or chair for the track the paper is in."
+        description = "Responds with the title and abstract of the given paper. "
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Successful retrieval of paper title and abstract"),
-        @ApiResponse(responseCode = "403", description = "Forbidden. The requester lacks necessary permissions.", content = {@Content(schema = @Schema())}),
+        @ApiResponse(responseCode = "403", description = "Forbidden. The requester must be a reviewer or chair for the track the paper is in.", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "404", description = "Not Found. The requested paper was not found.", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "500", description = "Internal Server Error. An unexpected server error occurred.", content = {@Content(schema = @Schema())})
     })
     @ResponseBody
     @GetMapping(path = "/title-and-abstract", produces = "application/json")
     public ResponseEntity<PaperSummary> getTitleAndAbstract(
-        @RequestParam Long requesterID,
-        @PathVariable Long paperID
+        @RequestParam @Parameter(description = "The ID of a user making the requests") Long requesterID,
+        @PathVariable @Parameter(description = "The ID of a paper to return") Long paperID
     ) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
     @Operation(summary = "Get the whole paper without the author names",
         description = "Responds all the contents of a paper submission. " +
             "Excludes the names and credentials of the author. " +
-            "The requester must be: a chair of the track the paper is in; or a reviewer " +
             "who has been assigned to the given paper (the review phase for the paper must have started)."
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Successful retrieval of the complete paper"),
-        @ApiResponse(responseCode = "403", description = "Forbidden. The requester lacks necessary permissions.", content = {@Content(schema = @Schema())}),
+        @ApiResponse(responseCode = "403", description = "Forbidden. The requester must be: a chair of the track the paper is in; or a reviewer ", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "404", description = "Not Found. The requested paper was not found.", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "500", description = "Internal Server Error. An unexpected server error occurred.", content = {@Content(schema = @Schema())})
     })
     @ResponseBody
     @GetMapping(path = "", produces = "application/json")
     public ResponseEntity<Paper> get(
-        @RequestParam Long requesterID,
-        @PathVariable Long paperID
+        @RequestParam @Parameter(description = "The ID of a user making the requests") Long requesterID,
+        @PathVariable @Parameter(description = "The ID of a paper to return") Long paperID
     ) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
@@ -69,15 +68,15 @@ public class PapersController {
     )
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "Successful retrieval of the paper review status"),
-        @ApiResponse(responseCode = "403", description = "Forbidden. The requester lacks necessary permissions.", content = {@Content(schema = @Schema())}),
+        @ApiResponse(responseCode = "403", description = "Forbidden. The requester must be an author of the paper, a chair of the paper's track, or a reviewer assigned to the paper.", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "404", description = "Not Found. The requested paper was not found.", content = {@Content(schema = @Schema())}),
         @ApiResponse(responseCode = "500", description = "Internal Server Error. An unexpected server error occurred.", content = {@Content(schema = @Schema())})
     })
     @ResponseBody
     @GetMapping(path = "/status", produces = "application/json")
     public ResponseEntity<PaperStatus> getState(
-        @RequestParam Long requesterID,
-        @PathVariable Long paperID
+        @RequestParam @Parameter(description = "The ID of a user making the requests") Long requesterID,
+        @PathVariable @Parameter(description = "The ID of a paper to check status") Long paperID
     ) {
         return new ResponseEntity<>(HttpStatus.NOT_IMPLEMENTED);
     }
