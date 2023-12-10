9277b033e67a717057e0cb083fe0c19971b47ec4
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-10 22:06:03 +0100

    Added papers service

---
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/application/PapersService.java
@@ -0,0 +1,24 @@
+package nl.tudelft.sem.v20232024.team08b.application;
+
+import nl.tudelft.sem.v20232024.team08b.repos.PaperRepository;
+import nl.tudelft.sem.v20232024.team08b.repos.ReviewRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class PapersService {
+    private final PaperRepository paperRepository;
+    private final ReviewRepository reviewRepository;
+
+    /**
+     * Default constructor for the service
+     * @param paperRepository repository storing the papers
+     * @param reviewRepository repository storing the reviews
+     */
+    @Autowired
+    public PapersService(PaperRepository paperRepository,
+                       ReviewRepository reviewRepository) {
+        this.paperRepository = paperRepository;
+        this.reviewRepository = reviewRepository;
+    }
+}
