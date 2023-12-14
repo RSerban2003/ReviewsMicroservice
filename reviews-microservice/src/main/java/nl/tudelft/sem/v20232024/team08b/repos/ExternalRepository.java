fcfaac43e41154dff762950b64e579b37a737da6
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-14 19:31:43 +0100

    Fixed checkstyle

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/repos/ExternalRepository.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/repos/ExternalRepository.java
@@ -5,7 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class ExternalRepository {
     /**
-     * Empty constructor
+     * Empty constructor.
      */
     public ExternalRepository() {
     }
