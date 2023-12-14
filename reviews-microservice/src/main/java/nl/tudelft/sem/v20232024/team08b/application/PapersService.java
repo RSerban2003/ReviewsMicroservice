de1d2b1348f76b6ffff52c4c5bceb52fd20e416d
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-14 19:06:44 +0100

    Added external repository

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/application/PapersService.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/application/PapersService.java
@@ -1,5 +1,6 @@
 package nl.tudelft.sem.v20232024.team08b.application;
 
+import nl.tudelft.sem.v20232024.team08b.repos.ExternalRepository;
 import nl.tudelft.sem.v20232024.team08b.repos.PaperRepository;
 import nl.tudelft.sem.v20232024.team08b.repos.ReviewRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,17 +10,22 @@ import org.springframework.stereotype.Service;
 public class PapersService {
     private final PaperRepository paperRepository;
     private final ReviewRepository reviewRepository;
+    private final ExternalRepository externalRepository;
 
     /**
      * Default constructor for the service.
      *
      * @param paperRepository repository storing the papers
      * @param reviewRepository repository storing the reviews
+     * @param externalRepository repository storing everything outside of
+     *                           this microservice
      */
     @Autowired
     public PapersService(PaperRepository paperRepository,
-                         ReviewRepository reviewRepository) {
+                         ReviewRepository reviewRepository,
+                         ExternalRepository externalRepository) {
         this.paperRepository = paperRepository;
         this.reviewRepository = reviewRepository;
+        this.externalRepository = externalRepository;
     }
 }
---
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/repos/ExternalRepository.java
@@ -0,0 +1,12 @@
+package nl.tudelft.sem.v20232024.team08b.repos;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ExternalRepository {
+    /**
+     * Empty constructor
+     */
+    public ExternalRepository() {
+    }
+}
