8dea11a2ea676104c7d394c7f026efb74f7270cc
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-15 19:43:50 +0100

    Updated tests again

--- reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/Tests.java
+++ reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/Tests.java
@@ -19,50 +19,50 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 public class Tests {
 
     MockMvc mockMvc;
-//    Mockito has to be used here. This is only an example
+    //    Mockito has to be used here. This is only an example
 
-//    @BeforeEach
-//    void setup() {
-////        this.mockMvc = MockMvcBuilders.standaloneSetup(new AssignmentsController()).build();
-//    }
-//
-//    @Test
-//    public void testGetEndpoints() throws Exception {
-//        String paperID = "123";
-//        mockMvc.perform(MockMvcRequestBuilders.get("/papers/{paperID}/assignees", paperID)
-//                .param("requesterID", "456")) // Add requesterID as a query parameter
-//            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
-//    }
-//
-//    @Test
-//    public void testPostEndpoints() throws Exception {
-//        String paperID = "123";
-//        String reviewerID = "123";
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .post("/papers/{paperID}/assignees/{reviewerID}", paperID, reviewerID)
-//                .param("requesterID", "456")) // Add requesterID as a query parameter
-//            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
-//    }
-//
-//    @Test
-//    public void remove() throws Exception {
-//        String paperID = "123";
-//        String reviewerID = "125";
-//
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .delete("/papers/{paperID}/assignees/{reviewerID}", paperID, reviewerID)
-//                .param("requesterID", "456")  // Add requesterID as a query parameter
-//                .contentType(MediaType.APPLICATION_JSON))  // Set content type to JSON
-//            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
-//    }
-//
-//    @Test
-//    public void put() throws Exception {
-//        String conferenceID = "125";
-//        String trackID = "126";
-//        mockMvc.perform(MockMvcRequestBuilders
-//                .put("/conferences/{conferenceID}/tracks/{trackID}/automatic", conferenceID, trackID)
-//                .param("requesterID", "456"))  // Add requesterID as a query parameter
-//            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
-//    }
+    //    @BeforeEach
+    //    void setup() {
+    ////        this.mockMvc = MockMvcBuilders.standaloneSetup(new AssignmentsController()).build();
+    //    }
+    //
+    //    @Test
+    //    public void testGetEndpoints() throws Exception {
+    //        String paperID = "123";
+    //        mockMvc.perform(MockMvcRequestBuilders.get("/papers/{paperID}/assignees", paperID)
+    //                .param("requesterID", "456")) // Add requesterID as a query parameter
+    //            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+    //    }
+    //
+    //    @Test
+    //    public void testPostEndpoints() throws Exception {
+    //        String paperID = "123";
+    //        String reviewerID = "123";
+    //        mockMvc.perform(MockMvcRequestBuilders
+    //                .post("/papers/{paperID}/assignees/{reviewerID}", paperID, reviewerID)
+    //                .param("requesterID", "456")) // Add requesterID as a query parameter
+    //            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+    //    }
+    //
+    //    @Test
+    //    public void remove() throws Exception {
+    //        String paperID = "123";
+    //        String reviewerID = "125";
+    //
+    //        mockMvc.perform(MockMvcRequestBuilders
+    //                .delete("/papers/{paperID}/assignees/{reviewerID}", paperID, reviewerID)
+    //                .param("requesterID", "456")  // Add requesterID as a query parameter
+    //                .contentType(MediaType.APPLICATION_JSON))  // Set content type to JSON
+    //            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+    //    }
+    //
+    //    @Test
+    //    public void put() throws Exception {
+    //        String conferenceID = "125";
+    //        String trackID = "126";
+    //        mockMvc.perform(MockMvcRequestBuilders
+    //                .put("/conferences/{conferenceID}/tracks/{trackID}/automatic", conferenceID, trackID)
+    //                .param("requesterID", "456"))  // Add requesterID as a query parameter
+    //            .andExpect(MockMvcResultMatchers.status().isNotImplemented());
+    //    }
 }
