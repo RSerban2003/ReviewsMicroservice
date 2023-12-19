8b44e83af7eaf100bcf123c609fdd8e511d76e2e
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-20 00:40:44 +0100

    Added tests for verification service

---
+++ reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/unit/services/VerificationServiceTests.java
@@ -0,0 +1,75 @@
+package nl.tudelft.sem.v20232024.team08b.unit.services;
+
+import javassist.NotFoundException;
+import nl.tudelft.sem.v20232024.team08b.application.VerificationService;
+import nl.tudelft.sem.v20232024.team08b.dtos.review.UserRole;
+import nl.tudelft.sem.v20232024.team08b.dtos.submissions.Submission;
+import nl.tudelft.sem.v20232024.team08b.dtos.users.RolesOfUser;
+import nl.tudelft.sem.v20232024.team08b.dtos.users.RolesOfUserTracksInner;
+import nl.tudelft.sem.v20232024.team08b.repos.ExternalRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import java.util.ArrayList;
+import java.util.List;
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.mockito.Mockito.when;
+
+public class VerificationServiceTests {
+    @MockBean
+    ExternalRepository externalRepository = Mockito.mock(ExternalRepository.class);
+    private VerificationService verificationService = new VerificationService(externalRepository);
+
+    private Submission fakeSubmission;
+    private RolesOfUser fakeRolesOfUser;
+
+    @BeforeEach
+    void prepare() {
+        fakeSubmission = new Submission();
+        fakeSubmission.setTrackId(3L);
+
+        RolesOfUserTracksInner inner = new RolesOfUserTracksInner();
+        inner.setRoleName("PC Member");
+        inner.setTrackId(2);
+        inner.setEventId(4);
+
+        List<RolesOfUserTracksInner> listOfTracks = new ArrayList<>();
+        listOfTracks.add(inner);
+
+        fakeRolesOfUser.setTracks(listOfTracks);
+        fakeRolesOfUser = new RolesOfUser();
+
+    }
+
+    @Test
+    void verifyPaperExists() throws NotFoundException {
+        when(externalRepository.getSubmission(1L)).thenReturn(fakeSubmission);
+        assertThat(verificationService.verifyPaper(1L)).isEqualTo(true);
+    }
+
+    @Test
+    void verifyPaperDoesNotExist() throws NotFoundException {
+        when(externalRepository.getSubmission(1L)).thenThrow(new NotFoundException(""));
+        assertThat(verificationService.verifyPaper(1L)).isEqualTo(false);
+    }
+
+    @Test
+    void verifyUserExists() throws NotFoundException {
+        when(externalRepository.getRolesOfUser(1L)).thenReturn(fakeRolesOfUser);
+        assertThat(verificationService.verifyUser(1L, 4L, 2L, UserRole.REVIEWER)).isEqualTo(true);
+    }
+
+    @Test
+    void verifyUserExistsButInDifferentConference() throws NotFoundException {
+        // This user IS a reviewer in a track with the same ID, but in a different conference
+        when(externalRepository.getRolesOfUser(1L)).thenReturn(fakeRolesOfUser);
+        assertThat(verificationService.verifyUser(1L, 3L, 2L, UserRole.REVIEWER)).isEqualTo(false);
+    }
+
+    @Test
+    void verifyUserDoesNotExist() throws NotFoundException {
+        when(externalRepository.getRolesOfUser(1L)).thenThrow(new NotFoundException(""));
+        assertThat(verificationService.verifyUser(1L, 4L, 2L, UserRole.REVIEWER)).isEqualTo(false);
+    }
+}
