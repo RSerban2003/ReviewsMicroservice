0cc368d55f38f52afe925494b4eb66f831b007a0
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-20 00:34:29 +0100

    Updated review repository namings

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/repos/ReviewRepository.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/repos/ReviewRepository.java
@@ -1,10 +1,10 @@
 package nl.tudelft.sem.v20232024.team08b.repos;
 
 import nl.tudelft.sem.v20232024.team08b.domain.Review;
-import nl.tudelft.sem.v20232024.team08b.domain.ReviewId;
+import nl.tudelft.sem.v20232024.team08b.domain.ReviewID;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ReviewRepository extends JpaRepository<Review, ReviewId> {
+public interface ReviewRepository extends JpaRepository<Review, ReviewID> {
 }
