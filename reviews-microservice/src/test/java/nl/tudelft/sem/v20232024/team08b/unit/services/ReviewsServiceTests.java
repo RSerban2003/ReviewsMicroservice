b15b21c3648a855ee7c1510d3e186df78ec8659d
Augustinas Jucas <augustinasjucas@gmail.com>
2023-12-20 14:48:29 +0100

    Refactored tests for readability

--- reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/unit/services/ReviewsServiceTests.java
+++ reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/unit/services/ReviewsServiceTests.java
@@ -43,6 +43,11 @@ public class ReviewsServiceTests {
     private nl.tudelft.sem.v20232024.team08b.dtos.review.Review reviewDTO;
     private Submission fakeSubmission;
     private Review fakeReview;
+    private Long requesterID = 0L;
+    private Long conferenceID = 1L;
+    private Long trackID = 2L;
+    private Long reviewerID = 3L;
+    private Long paperID = 4L;
     @BeforeEach
     void prepare() {
         reviewDTO = new nl.tudelft.sem.v20232024.team08b.dtos.review.Review(
@@ -55,75 +60,75 @@ public class ReviewsServiceTests {
         fakeReview = new Review();
 
         fakeSubmission = new Submission();
-        fakeSubmission.setTrackId(3L);
-        fakeSubmission.setEventId(4L);
+        fakeSubmission.setTrackId(trackID);
+        fakeSubmission.setEventId(conferenceID);
     }
 
     @Test
     void submitReviewNoSuchPaper() {
-        when(verificationService.verifyPaper(2L)).thenReturn(false);
+        when(verificationService.verifyPaper(paperID)).thenReturn(false);
         Assert.assertThrows(NotFoundException.class, () -> {
-            reviewsService.submitReview(reviewDTO, 1L, 2L);
+            reviewsService.submitReview(reviewDTO, requesterID, paperID);
         });
     }
 
     @Test
     void submitReviewNoSuchUser() throws NotFoundException {
         // Assume first IF works
-        when(verificationService.verifyPaper(2L)).thenReturn(true);
+        when(verificationService.verifyPaper(paperID)).thenReturn(true);
 
         // Essentially, fake the track, that the submission belongs to
-        when(externalRepository.getSubmission(2L)).thenReturn(fakeSubmission);
+        when(externalRepository.getSubmission(paperID)).thenReturn(fakeSubmission);
 
         // Assume the second IF does not work
-        when(verificationService.verifyUser(1L, 4L, 3L, UserRole.REVIEWER))
+        when(verificationService.verifyUser(requesterID, conferenceID, trackID, UserRole.REVIEWER))
                 .thenReturn(false);
 
         Assert.assertThrows(IllegalCallerException.class, () -> {
-            reviewsService.submitReview(reviewDTO, 1L, 2L);
+            reviewsService.submitReview(reviewDTO, requesterID, paperID);
         });
     }
 
     @Test
     void submitReviewNotAReviewer() throws NotFoundException {
         // Assume first IF works
-        when(verificationService.verifyPaper(2L)).thenReturn(true);
+        when(verificationService.verifyPaper(paperID)).thenReturn(true);
 
         // Essentially, fake the track, that the submission belongs to
-        when(externalRepository.getSubmission(2L)).thenReturn(fakeSubmission);
+        when(externalRepository.getSubmission(paperID)).thenReturn(fakeSubmission);
 
         // Assume the second IF works
-        when(verificationService.verifyUser(1L, 4L, 3L, UserRole.REVIEWER))
+        when(verificationService.verifyUser(requesterID, conferenceID, trackID, UserRole.REVIEWER))
                 .thenReturn(true);
 
         // Assume that the user is not a reviewer - i.e., the third IF does not work
-        when(reviewRepository.findById(new ReviewID(1L, 2L)))
+        when(reviewRepository.findById(new ReviewID(paperID, reviewerID)))
                 .thenReturn(Optional.empty());
 
         Assert.assertThrows(IllegalAccessException.class, () -> {
-            reviewsService.submitReview(reviewDTO, 1L, 2L);
+            reviewsService.submitReview(reviewDTO, requesterID, paperID);
         });
     }
 
     @Test
     void submitReviewSuccessful() throws Exception {
         // Assume first IF works
-        when(verificationService.verifyPaper(2L)).thenReturn(true);
+        when(verificationService.verifyPaper(paperID)).thenReturn(true);
 
         // Essentially, fake the track, that the submission belongs to
-        when(externalRepository.getSubmission(2L)).thenReturn(fakeSubmission);
+        when(externalRepository.getSubmission(paperID)).thenReturn(fakeSubmission);
 
         // Assume the second IF works
-        when(verificationService.verifyUser(1L, 4L, 3L, UserRole.REVIEWER))
+        when(verificationService.verifyUser(requesterID, conferenceID, trackID, UserRole.REVIEWER))
                 .thenReturn(true);
 
         // Assume that the third if works
-        when(reviewRepository.findById(new ReviewID(1L, 2L)))
+        when(reviewRepository.findById(new ReviewID(paperID, requesterID)))
                 .thenReturn(Optional.of(fakeReview));
 
         // Assert that a correct review is added to the repository
-        Review expected = new Review(reviewDTO, new ReviewID(2L, 1L));
-        reviewsService.submitReview(reviewDTO, 1L, 2L);
+        Review expected = new Review(reviewDTO, new ReviewID(paperID, requesterID));
+        reviewsService.submitReview(reviewDTO, requesterID, paperID);
         verify(reviewRepository).save(expected);
     }
 }
