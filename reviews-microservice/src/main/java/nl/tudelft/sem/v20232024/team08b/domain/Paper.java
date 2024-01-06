484735e5c4ad711d96af9abd6cc07f404800f4ee
Augustinas Jucas <augustinasjucas@gmail.com>
2024-01-06 21:49:17 +0100

    Removed redundant information from Paper class

--- reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/domain/Paper.java
+++ reviews-microservice/src/main/java/nl/tudelft/sem/v20232024/team08b/domain/Paper.java
@@ -3,16 +3,9 @@ package nl.tudelft.sem.v20232024.team08b.domain;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import nl.tudelft.sem.v20232024.team08b.dtos.review.PaperPhase;
-import nl.tudelft.sem.v20232024.team08b.dtos.review.PaperStatus;
+
+import javax.persistence.*;
 import java.io.Serializable;
-import java.util.List;
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 
 @Entity
 @Data
@@ -23,13 +16,12 @@ public class Paper implements Serializable {
     @GeneratedValue
     private Long id;
 
-    @OneToMany(cascade = CascadeType.ALL)
-    private List<Review> listOfReviews;
-
     @ManyToOne(cascade = CascadeType.ALL)
     private Track track;
 
     private PaperStatus status;
 
-    private PaperPhase phase;
+    // TODO: make sure that when finalize is called
+    //       on a paper, that this is set to true
+    private Boolean reviewsHaveBeenFinalized = false;
 }
