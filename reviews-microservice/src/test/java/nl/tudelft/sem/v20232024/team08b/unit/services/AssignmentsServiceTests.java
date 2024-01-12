488fb11ed901c44dfb0ab6126ac984f6561fffa4
jrzwrz <j.k.wierzbicki@student.tudelft.nl>
2024-01-13 00:00:19 +0100

    checkstyle

--- reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/unit/services/AssignmentsServiceTests.java
+++ reviews-microservice/src/test/java/nl/tudelft/sem/v20232024/team08b/unit/services/AssignmentsServiceTests.java
@@ -13,8 +13,6 @@ import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.verifyNoInteractions;
-import static org.mockito.Mockito.verifyNoMoreInteractions;
 import static org.mockito.Mockito.when;
 
 import java.util.ArrayList;
